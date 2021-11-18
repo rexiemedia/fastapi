9a46274f1af8e3c6eadf78094b1bae7b2bc10ede
Rex Otavotoma <rexiemedia01@gmail.com>
2021-11-18 10:54:38 +0100

    error handling

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body 
+
 # importing basemodel to create a schema for incoming data
 from pydantic import BaseModel
 from random import randrange
+
+from starlette.responses import Response
 
 app = FastAPI()
 
@@ -18,7 +21,7 @@ all_posts = [{
     "title": "fastapi 1",
     "content": "checking it up",
     "id": 1
-}, {
+},{
     "title": "fastapi 2",
     "content": "checking it up",
      "id": 2
@@ -28,8 +31,8 @@ def find_post(id):
     for p in all_posts:
         if p["id"] == id:
             return p
-        else:
-            return {"msg": "Not found"}
+        # else:
+        #     return [{"msg": f"details with {id} Not found"}, {"statuscode": 404}]
 
 @app.get("/")
 async def root():
@@ -40,8 +43,12 @@ def get_posts():
     return {"data": all_posts}
 
 @app.get("/posts/{id}")
-def get_post(id: int):
+def get_post(id: int, response: Response):
     post = find_post(id)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"Post with id: {id} was not found")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {"message": f"Post with id: {id} was not found"}
     return {"post_details": post}
 
 @app.post("/posts")
