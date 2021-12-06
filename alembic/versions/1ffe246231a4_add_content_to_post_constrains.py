36a9039ee33e4910f1482d663377c3de9e6a5772
Rex Otavotoma <rexiemedia01@gmail.com>
2021-12-06 15:33:01 +0100

    alembic post user_id updated

--- alembic/versions/1ffe246231a4_add_content_to_post_constrains.py
+++ alembic/versions/1ffe246231a4_add_content_to_post_constrains.py
@@ -19,11 +19,11 @@ depends_on = None
 def upgrade():
     op.add_column('posts', sa.Column('owner_id', sa.Integer(), nullable=False))
     op.create_foreign_key('post_users_fk', source_table="posts", referent_table="users", local_cols=[
-                          'owner_id'], remote_cols=['id'], ondelete="CASCADE")
+                          'user_id'], remote_cols=['id'], ondelete="CASCADE")
     pass
 
 
 def downgrade():
     op.drop_constraint('post_users_fk', table_name="posts")
-    op.drop_column('posts', 'owner_id')
+    op.drop_column('posts', 'user_id')
     pass
